0530a48980fcbb88a3d2047b87464dd2ee5c5063
Kennedy Mutugi Julius <kennedymutugi111@gmail.com>
2024-04-02 10:30:16 +0300

    added tracking map

---
+++ lib/screens/other_screens/order_tracking_screen.dart
@@ -0,0 +1,29 @@
+// import 'dart:async';
+
+// import 'package:flutter/material.dart';
+
+// class OrderTrackingScreen extends StatefulWidget {
+//   const OrderTrackingScreen({super.key});
+
+//   @override
+//   State<OrderTrackingScreen> createState() => _OrderTrackingScreenState();
+// }
+
+// final Completer<GoogleMapController> _controller = Completer();
+
+// static const 
+
+// class _OrderTrackingScreenState extends State<OrderTrackingScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           "Track Oder",
+//           style: TextStyle(color: Colors.black, fontSize: 16),
+//         ),
+//       ),
+//       body: GoogleMap(),
+//     );
+//   }
+// }
